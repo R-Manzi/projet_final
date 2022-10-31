404d1445b882ad9305c573400adf9b9c9abe13a4
R-Manzi <111302767+R-Manzi@users.noreply.github.com>
2022-10-31 04:04:12 -0400

    Update itulufu.py

--- itulufu.py
+++ itulufu.py
@@ -1,6 +1,63 @@
 
+import random
 
 
 
-for i in range(11):
-    print(i)
+
+class Carte:
+    SUITS = ['♠','♦','♥','♣']
+    VALUES= ['A','3','4','5','6','7','J','Q','K']
+    
+
+class Paquet:
+    
+    def __init__(self):
+        self.paquet = creation_paquet()
+
+    def afficher(self):
+        liste = []
+        for element in self.paquet:
+            liste.append(element)
+        print(liste)
+
+    
+    
+    def shuffle(self):
+        random.shuffle(self.paquet)
+
+
+    
+    def enlever_carte(self):
+        return self.paquet.pop()
+
+
+
+
+class joueur (Paquet):
+    pass
+
+
+
+class Classement:
+    pass
+    
+
+
+class Points:
+    POINTS= [("Q",2),("J",3),("K",4),("7",11),("A",11)]
+
+
+
+def creation_paquet():
+    paquet= []
+    for i in range((len(Carte.SUITS))):
+        for suit in Carte.SUITS:
+            for value in Carte.VALUES:
+                paquet.append(value+suit)
+    return paquet
+
+b= Paquet()
+b.afficher()
+b.shuffle()
+print()
+b.afficher()
