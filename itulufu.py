4fe5b7a80d822081825d32376fd2285309bb9956
R-Manzi <111302767+R-Manzi@users.noreply.github.com>
2022-10-31 05:16:02 -0400

    points global, class mere

--- itulufu.py
+++ itulufu.py
@@ -2,6 +2,39 @@
 import random
 
 
+POINTS_CARTES_JEU = { '3':3,'4':4,'5':5,'6':6,'Q':7,'J':8,'K':9,'7':10,"A":11, }
+
+POiNTS_FIN_PARTIE = { '3':0,'4':0,'5':0,'6':0,'Q':2,'J':3,'K':4,'7':10,"A":11, }
+
+
+
+class Itulufu:
+
+    def __init__(self) -> None:
+
+        self.cartes_joueur = []
+        self.cartes_ordi = []
+        self.pile_cartes = []
+        self.carte_reference = []
+        
+        self.pile_point_joueur = []
+        self.pile_point_ordi = []
+
+
+        def distribuer_cartes(self):
+            pass
+
+        def piger_cartes(self):
+            pass
+
+        def comparer_paquet(self):
+            pass
+
+
+
+
+
+
 
 
 class Carte:
@@ -10,8 +43,10 @@ class Carte:
     
 
 class Paquet:
+
+    POINTS= [("Q",2),("J",3),("K",4),("7",11),("A",11)]
     
-    def __init__(self):
+    def __init__(self): 
         self.paquet = creation_paquet()
 
     def afficher(self):
@@ -25,27 +60,29 @@ class Paquet:
     def shuffle(self):
         random.shuffle(self.paquet)
 
-
     
     def enlever_carte(self):
         return self.paquet.pop()
 
+    
 
 
 
-class joueur (Paquet):
-    pass
 
+class JoueurMain (Paquet):
+    def __init__(self, id_joueur=""):
+        self.cartes = []
+        self.id_joeur = id
+
+
+
+
+ 
 
 
 class Classement:
     pass
     
-
-
-class Points:
-    POINTS= [("Q",2),("J",3),("K",4),("7",11),("A",11)]
-
 
 
 def creation_paquet():
@@ -56,6 +93,17 @@ def creation_paquet():
                 paquet.append(value+suit)
     return paquet
 
+
+
+def demarrer():
+
+
+
+
+
+
+
+ 
 b= Paquet()
 b.afficher()
 b.shuffle()
